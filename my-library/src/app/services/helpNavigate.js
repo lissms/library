8e6db301423c74edc81326cc28af1d7598f0bad1
Lissandra <70586226+lissms@users.noreply.github.com>
2021-12-02 21:57:06 +0100

    delete commmit

--- my-library/src/app/services/helpNavigate.js
+++ my-library/src/app/services/helpNavigate.js
@@ -1,43 +1,28 @@
 // TODO
 // Cambiar nombre del archivo a navigationHelper.js
-// Borrar todos los comentarios, no son necesarios porque la funciones estan bien nombradas
 // Cambiar el nombre de los parámetros "props" por uno más especifico
 // Sacar este archivo de la carpeta service, y ponerlo dentro de uno de que se llame helpers
 
-//goToBookList
 export const goToBookList = (history) => {
   history.push(`/`);
 };
 
-//handleAddClick
 export const goToAddBookPage = (history) => {
   history.push(`/book/add`);
 };
 
-//goToAddAuthorPage
 export const goToAddAuthorPage = (history) => {
   history.push(`/author/add`);
 };
 
-//goToBookDetail
 export const goToBookDetail = (history, props) => {
   history.push(`/book/detail/${props}`);
 };
 
-//goToUpdateBookPage
 export const goToUpdateBookPage = (history, props) => {
   history.push(`/book/update/${props}`);
 };
 
-//goToUpdateAuthorPage
 export const goToUpdateAuthorPage = (history, props) => {
   history.push(`/author/update/${props}`);
 };
-
-/* export const handleAddClick = (history, props) => {
-  history.push(`/book/update/${props}`);
-}; */
-
-/* export const handleRemoveClick = (history, props) => {
-  history.push(`/book/update/${props}`);
-}; */

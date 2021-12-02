b56e56dfe7d15cff4b8daa7a028f29d5cafcd708
Lissandra <70586226+lissms@users.noreply.github.com>
2021-12-02 08:05:37 +0100

    add updateautor styled

---
+++ my-library/src/app/components/update/updateAuthor.styled.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const UpdateContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 88vh;
+`;
+
+export const UpdateStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background: #ffffff8c;
+  padding: 32px;
+  border: 1px solid #f17680;
+  border-radius: 4px;
+
+  & .title {
+    color: #6d5a7f;
+  }
+
+  & .from {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    margin-top: 32px;
+
+    & label {
+      font-size: 12px;
+    }
+  }
+
+  & .update-name-label {
+    margin-bottom: 4px;
+  }
+  & .update-birthday-label {
+    margin-bottom: 4px;
+    margin-top: 10px;
+  }
+
+  & input {
+    width: 100%;
+    border-radius: 4px;
+    padding: 6px;
+    border: none;
+    box-sizing: border-box;
+  }
+
+  & .button-save {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 7vh;
+    background: gray;
+    width: 100%;
+    cursor: pointer;
+    margin-top: 16px;
+    background: #f8b18e;
+    border: 2px solid #6d5a7f;
+    transition: all 0.5s;
+    color: #6d5a7f;
+    font-weight: bold;
+  }
+
+  & .button-save:disabled {
+    background: #c5c5c5;
+    border: 2px solid #969696;
+    color: #969696;
+  }
+`;
